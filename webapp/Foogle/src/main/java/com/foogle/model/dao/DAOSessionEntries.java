d139342e02f9f6562bf20c36ee59c330a0ad74df
DaemonWar <charbonnier.franck@gmail.com>
2015-02-06 20:47:43 +0100

    Fin de la recommandation and... IT WORKS !!!!

--- webapp/Foogle/src/main/java/com/foogle/model/dao/DAOSessionEntries.java
+++ webapp/Foogle/src/main/java/com/foogle/model/dao/DAOSessionEntries.java
@@ -1,24 +1,31 @@
 package com.foogle.model.dao;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.foogle.model.entities.SearchEntries;
 import com.foogle.model.entities.SessionEntries;
 
 public class DAOSessionEntries extends DAO<SessionEntries>
 {
-	public ArrayList<SearchEntries> getRecommandedEntries(Integer entryId, int maxResult)
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public List<SearchEntries> getRecommandedEntries(Integer entryId, int maxResult)
 	{
 		try
 		{
-			return (ArrayList<SearchEntries>) em
+			return (List<SearchEntries>) em
 					.createQuery(
-							"SELECT DISTINCT se.queryId FROM sessionEntries se WHERE se.queryId.id <> " + entryId
-									+ " AND se.sessionId IN (SELECT se2.sessionId FROM sessionEntries se2 WHERE se2.queryId.id = " + entryId
-									+ ") ORDER BY se.queryId.count DESC").setMaxResults(maxResult).getResultList();
+							"SELECT 1, se.queryId.query, se.queryId.count FROM SessionEntries se WHERE se.queryId.id <> " + entryId
+									+ " AND se.sessionId IN (SELECT se2.sessionId FROM SessionEntries se2 WHERE se2.queryId.id = " + entryId
+									+ ") GROUP BY se.queryId.query ORDER BY se.queryId.count DESC").setMaxResults(maxResult).getResultList();
 		} catch (Exception e)
 		{
-			return null;
+			logger.info(e.getMessage());
+			return new ArrayList<SearchEntries>();
 		}
 	}
 
